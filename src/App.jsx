6c9205127f35a4ed1f2ea18cc1df1093ffc42497
Lenard Paraiso <132863645+lenard8@users.noreply.github.com>
2025-05-03 13:17:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./Components/Home";
-import { ComponentCharacter } from './Components/ComponentCharacter';
-import { CmpTourGuides } from './Components/CmpTourGuides';
-import { CmpTravelAgentQuiz } from './Components/CmpTravelAgentQuiz';
-import { CmpHotelStaffQuiz } from './Components/CmpHotelStaffQuiz';
-import { CmpFlightAttendantQuiz } from './Components/CmpFlightAttendantQuiz';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Homepage from "./Components/Home";
+// import { ComponentCharacter } from './Components/ComponentCharacter';
+// import { CmpTourGuides } from './Components/CmpTourGuides';
+// import { CmpTravelAgentQuiz } from './Components/CmpTravelAgentQuiz';
+// import { CmpHotelStaffQuiz } from './Components/CmpHotelStaffQuiz';
+// import { CmpFlightAttendantQuiz } from './Components/CmpFlightAttendantQuiz';
 
-function App() {
-  return (
-    // <BrowserRouter basename="/"> 
-      <Routes>
-{/*         <Route path="/" element={<Homepage />} />  */}
-        <Route path="/character" element={<ComponentCharacter />} />
-        <Route path="/tourguide" element={<CmpTourGuides />} />
-        <Route path="/travelagent" element={<CmpTravelAgentQuiz />} />
-        <Route path="/hotelstaff" element={<CmpHotelStaffQuiz />} />
-        <Route path="/flightattendant" element={<CmpFlightAttendantQuiz />} />
-      </Routes>
-    </BrowserRouter>
-  );
+// function App() {
+//   return (
+//     <BrowserRouter basename="/"> 
+//       <Routes>
+//         <Route path="/" element={<Homepage />} /> 
+//         <Route path="/character" element={<ComponentCharacter />} />
+//         <Route path="/tourguide" element={<CmpTourGuides />} />
+//         <Route path="/travelagent" element={<CmpTravelAgentQuiz />} />
+//         <Route path="/hotelstaff" element={<CmpHotelStaffQuiz />} />
+//         <Route path="/flightattendant" element={<CmpFlightAttendantQuiz />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
 
-}
+// }
 
-export default App;
+// export default App;
